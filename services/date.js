e65fb839ece0326da39f78fbffa727abc4d2ee4c
Vladislav <colacat1927@gmail.com>
2022-02-18 16:10:07 +0200

    -

--- services/date.js
+++ services/date.js
@@ -1,25 +1,24 @@
 module.exports = function date() {
-    let date_ob = new Date();
+    return new Date().toLocaleString("ru-RU", {timeZone: "Europe/Moscow"});
 
     // current date
     // adjust 0 before single digit date
-    let date = ("0" + date_ob.getDate()).slice(-2);
+    // let date = ("0" + date_ob.getDate()).slice(-2);
 
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    // // current month
+    // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 
-    // current year
-    let year = date_ob.getFullYear();
+    // // current year
+    // let year = date_ob.getFullYear();
 
-    // current hours
-    let hours = date_ob.getHours();
+    // // current hours
+    // let hours = date_ob.getHours();
 
-    // current minutes
-    let minutes = date_ob.getMinutes();
+    // // current minutes
+    // let minutes = date_ob.getMinutes();
 
-    // current seconds
-    let seconds = date_ob.getSeconds();
+    // // current seconds
+    // let seconds = date_ob.getSeconds();
 
-    return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
-
+    // return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
 }
